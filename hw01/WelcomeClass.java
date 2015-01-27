0c46e731fc0e2b07ae533451828bab8a87a918e7
erv318 <erv318@lehigh.edu>
2015-01-27 17:12:41 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,17 @@
+ //define a class
+ public class WelcomeClass{
+ 
+ //add main method
+ public static void main(String[] args) {
+ 
+  System.out.println("   -----------");
+  System.out.println("   | WELCOME |");
+  System.out.println("   -----------");
+  System.out.println("  ^  ^  ^  ^  ^  ^");
+  System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+  System.out.println("<-E--R--V--3--1--8->");
+  System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+  System.out.println("  v  v  v  v  v  v");
+  System.out.println(" I go to lehigh and am in the school of Engineering.");
+ }
+ }
